f7620b309ed0f4543abf8d640b64e47e0c66389f
Cargoo <cargoo@sina.com>
2018-07-11 11:45:53 +0800

    Initialize Ansible Module

---
+++ ansible/src/test/java/org/cargoo/ansible/test/MainTestAnsible.java
@@ -0,0 +1,18 @@
+package org.cargoo.ansible.test;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class MainTestAnsible {
+
+    @Before
+    public void setUp() {
+
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
