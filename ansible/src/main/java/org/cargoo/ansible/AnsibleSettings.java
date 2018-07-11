88d9a8b812115d3e029b1816c54bf3fc8ccd48ba
Cargoo <cargoo@sina.com>
2018-07-11 12:18:49 +0800

    Initialize Ansible Module

---
+++ ansible/src/main/java/org/cargoo/ansible/AnsibleSettings.java
@@ -0,0 +1,39 @@
+package org.cargoo.ansible;
+
+
+
+//@Component
+//@ConfigurationProperties(prefix = "ansible")
+
+public class AnsibleSettings {
+    private String tomcatPlaybookPath;
+
+    private String weblogicPlaybookPath;
+
+    private String ansibleHostsPath;
+
+    public String getTomcatPlaybookPath() {
+        return tomcatPlaybookPath;
+    }
+
+    public void setTomcatPlaybookPath(String tomcatPlaybookPath) {
+        this.tomcatPlaybookPath = tomcatPlaybookPath;
+    }
+
+    public String getWeblogicPlaybookPath() {
+        return weblogicPlaybookPath;
+    }
+
+    public void setWeblogicPlaybookPath(String weblogicPlaybookPath) {
+        this.weblogicPlaybookPath = weblogicPlaybookPath;
+    }
+
+    public String getAnsibleHostsPath() {
+        return ansibleHostsPath;
+    }
+
+    public void setAnsibleHostsPath(String ansibleHostsPath) {
+        this.ansibleHostsPath = ansibleHostsPath;
+    }
+
+}
